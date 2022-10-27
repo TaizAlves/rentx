f4ba21e31a99aeeb62730ca59fabd2cd6e0bdd3a
Taiz Alves <taizralves@gmail.com>
2022-10-27 16:01:21 -0300

    fix avaliable that was not changing then a rental was in progress

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -2,6 +2,7 @@ interface IDateProvider {
   compareHours(start_date: Date, end_date: Date): number;
   convertToUTC(date: Date): string;
   dateNow(): Date;
+  compareinDays(start_date: Date, end_date: Date ): number;
 }
 
 export { IDateProvider };
