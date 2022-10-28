74ead096d86ca48a0b213e7c043564a39aa58dfd
Taiz Alves <taizralves@gmail.com>
2022-10-28 16:14:31 -0300

    list rentals by user ok

--- README.md
+++ README.md
@@ -78,3 +78,14 @@ Ao realizar a devolução, o usuário deverá ser liberado para outro aluguel
 Ao realizar a devolução, deverá ser calculado o total do aluguel
 Caso o horário de devolução seja superior ao horário previsto de entrega, deverá ser cobrado multa proporcional ao período de atraso
 Caso haja multa, deverá ser somado ao total do aluguel
+
+
+# Listagem de Alugueis para usuário
+
+**RF**
+Deve ser possível realizar a busca de todos os alugueis feitos pelo usuário
+
+**RN**
+O usuário deve estar logado
+
+
--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,11 +1,28 @@
-import { CreateRentalController } from "@modules/rentals/useCase/createRental/CreateRentalController";
 import { Router } from "express";
+
+import { CreateRentalController } from "@modules/rentals/useCase/createRental/CreateRentalController";
+import { DevolutionRentalController } from "@modules/rentals/useCase/devolutionRental/DevolutionRentalController";
+import { ListRentalsByUserController } from "@modules/rentals/useCase/listRentalsByUser/ListRentalsByUserController";
+
 import { ensureAutenticated } from "../middleware/ensureAuthenticated";
 
 const rentalRoutes = Router();
 
-const createRentalController = new CreateRentalController()
+const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUserController = new ListRentalsByUserController();
 
 rentalRoutes.post("/", ensureAutenticated, createRentalController.handle);
+rentalRoutes.post(
+  "/devolution/:id",
+  ensureAutenticated,
+  devolutionRentalController.handle
+);
+
+rentalRoutes.get(
+  "/user",
+  ensureAutenticated,
+  listRentalsByUserController.handle
+);
 
 export { rentalRoutes };
