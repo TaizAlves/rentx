35436146755997f24b819e2244c0e1f0323f601e
Taiz Alves <taizralves@gmail.com>
2022-10-30 21:39:31 -0300

    starting implementation of refresh token

---
+++ src/modules/accounts/repositories/IUsersTokensRepository.ts
@@ -0,0 +1,13 @@
+import { ICreateUsersTokensDTO } from "../dtos/ICreateUsersTokensDTO";
+import { UsersTokens } from "../infra/typeorm/entities/UsersTokens";
+
+
+interface IUsersTokensRepository {
+  create({
+    expires_date,
+    refresh_token,
+    user_id,
+  }: ICreateUsersTokensDTO): Promise<UsersTokens>;
+}
+
+export { IUsersTokensRepository };
